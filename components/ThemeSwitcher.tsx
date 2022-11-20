6be793fe31e12385cbb909dd52fa789d9604ec2d
tlam67 <tlamottawa@gmail.com>
2022-11-20 01:07:02 -0500

    changed theme toggle from text to icon button

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ThemeSwitcher = () => {
   const [mounted, setMounted] = useState(false);
@@ -17,9 +19,16 @@ const ThemeSwitcher = () => {
     theme == 'light' ? setTheme('dark') : setTheme('light');
   }
 
+  const renderThemeIcon = () => {
+    if (theme === 'light') {
+      return <FontAwesomeIcon icon={faMoon} />;
+    }
+    return <FontAwesomeIcon icon={faSun} />
+  }
+
   return (
-    <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')} className="items-end">
-      Toggle theme
+    <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')} className="w-9 h-9 rounded-lg bg-amber-100 dark:bg-blue-black hover:animate-pulse" aria-label="toggle theme">
+      {renderThemeIcon()}
     </button>
   )
 
