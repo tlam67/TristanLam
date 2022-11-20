f23b5ec160f77faaa63e90309220db25b51c1dc3
tlam67 <tlamottawa@gmail.com>
2022-11-20 17:58:29 -0500

    added note section for future animation idea

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,20 +5,32 @@ import { ThemeProvider } from 'next-themes'
 import { lightTheme, darkTheme, GlobalStyles } from '../ThemeConfig'
 import ThemeSwitcher from '../components/ThemeSwitcher'
 import Header from '../components/Header'
-import About from '../components/about'
+import About from '../components/About'
 
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import Experience from '../components/Experience'
+import Projects from '../components/Projects'
 
 interface themedAppProps extends Omit<AppProps, "Component"> {
   Component: AppProps["Component"] & { theme: string | undefined }
 }
+
+/*
+Ideas for animations:
+fade in my name first, then the subtitle, then left to right fade in of header and opacity 0 to 100 on the bouncing arrow
+*/
 
 export default function App({ Component, pageProps }: themedAppProps) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
       <Header/>
       <Component {...pageProps} />
-      <About/>
+      <div className='min-h-screen'>
+        <About/>
+        <Experience/>
+        <Projects/>
+      </div>
+      
     </ThemeProvider>
   )
 }
