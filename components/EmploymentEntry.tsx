f2b45fff076a2a59e95eae15528e88c61da36e8c
tlam67 <tlamottawa@gmail.com>
2022-12-03 19:08:30 -0500

    added experience section

---
+++ components/EmploymentEntry.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react'
+import Image, { StaticImageData } from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+
+interface EmploymentInfo {
+  company: string,
+  position: string,
+  date: string,
+  logo: StaticImageData,
+  logoWidth: number,
+  logoHeight: number,
+  description: string,
+  href: string,
+}
+
+const EmploymentEntry = (employmentInfo: EmploymentInfo) => {
+  const [showDetails, setShowDetails] = useState(false);
+  
+  const toggleShowDetails = () => setShowDetails(!showDetails);
+
+  return (
+    <>
+    <div className='flex flex-row items-center mb-4 w-[45vh] max-w-2xl min-w-[50%] cursor-pointer' id="employment-entry" onClick={toggleShowDetails}>
+      <a href={employmentInfo.href}>
+        <Image
+          alt={employmentInfo.company + " logo"}
+          height={employmentInfo.logoHeight}
+          width={employmentInfo.logoWidth}
+          src={employmentInfo.logo}
+          priority
+          className={"mr-4 p-2 rounded-lg"}
+        />
+      </a>
+      <div className="w-7/24 justify-self-start">
+        <div className="dark:text-white text-gray-800 ">
+          {employmentInfo.company}
+        </div>
+        <span className="dark:text-gray-400 whitespace-nowrap text-gray-600 text-sm ">
+          {employmentInfo.position}
+        </span>
+      </div>
+      <span className="grow opacity-0 md:opacity-100  border-t dark:border-gray-500 mx-4 mb-4 self-end border-dashed shrink border-gray-400"></span>
+
+      <span className="dark:text-gray-400 sm:whitespace-nowrap whitespace-normal text-right text-gray-600 text-sm mt-2 justify-self-end">
+        {employmentInfo.date}
+      </span>
+
+      <FontAwesomeIcon icon={faAngleRight} size="1x" className={'mt-2 ml-2 animate-pulse duration-750 ' + (showDetails ? 'rotate-90' : 'hover:rotate-90')}/>
+    </div>
+    <div className='flex flex-row items-center mb-4 w-[45vh] max-w-2xl min-w-[50%] cursor-pointer' id="employment-entry" onClick={toggleShowDetails}>
+      {showDetails ? <p className='mb-4 ml-[4.75rem] break-all justify-self-start'>{employmentInfo.description}</p> : null}
+    </div>
+    </>
+  );
+}
+
+export default EmploymentEntry;
--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,7 +1,15 @@
 import styles from '../styles/Home.module.css'
 import profile from '../public/temp.jpg'
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 import { ReactElement, useState } from 'react'
+import pic from '../public/temp.jpg'
+import arcturusLogo from '../public/Arcturus-logo-light.svg'
+import wsibLogo from '../public/wsib-logo.svg'
+import hopinLogo from '../public/hopin-logo.svg'
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import EmploymentEntry from './EmploymentEntry';
 
 const titles = [
   'Software Engineer',
@@ -10,63 +18,64 @@ const titles = [
   'Learner',
 ]
 
-interface DescriptionTypes {
-  tldr: ReactElement,
-  less: ReactElement,
-  default: ReactElement,
-  more: ReactElement,
+interface EmploymentInfo {
+  company: string,
+  position: string,
+  date: string,
+  logo: StaticImageData,
+  logoWidth: number,
+  logoHeight: number,
+  description: string,
+  href: string,
 }
 
-const descriptions: DescriptionTypes = {
-  tldr: <div><p>tldr</p></div>,
-  less: <p>less</p>,
-  default: <p>default</p>,
-  more: <p>more</p>,
-}
+const employments: EmploymentInfo[] = [
+  {
+    company: "Arcturus Networks",
+    position: "Full Stack Software Engineering Intern",
+    date: "Fall 2022",
+    logo: arcturusLogo,
+    logoWidth: 60,
+    logoHeight: 60,
+    description: "arcturus description arcturus description arcturus description arcturus description arcturus description arcturus description arcturus description arcturus description arcturus description arcturus description arcturus description ",
+    href: "https://www.arcturusnetworks.com/",
+  },
+  {
+    company: "WSIB Innovation Lab",
+    position: "Full Stack Software Engineering Intern",
+    date: "Winter 2022",
+    logo: wsibLogo,
+    logoWidth: 60,
+    logoHeight: 60,
+    description: "wsib description",
+    href: "https://www.wsib-lab.ca/",
+  },
+  {
+    company: "Hop In",
+    position: "Full Stack Software Engineering Intern",
+    date: "Summer 2021",
+    logo: hopinLogo,
+    logoWidth: 60,
+    logoHeight: 60,
+    description: "hop in description",
+    href: "https://www.hopintech.com/",
+  },
+]
 
-const selectedButtonStyle = 'ring-2 rounded-md dark:ring-arrow-blue ring-black'
-
-const Description = (descriptionType: string, setDescriptionType: (descriptionType: string) => void) => {
-
-  const selectDescription = (type: string) =>  {
-    if (type === 'tldr') {
-      return descriptions.tldr;
-    } else if (type === 'less') {
-      return descriptions.less;
-    } else if (type === 'more') {
-      return descriptions.more;
-    }
-    return descriptions.default;
-  }
-
-  return (
-    <div className=''>
-      <div className='mt-3 grid grid-cols-4 gap-4'>
-        <button className={` m-1 p-1 ${descriptionType === 'tldr' ? selectedButtonStyle : undefined}`} onClick={() => setDescriptionType('tldr')}>TL;DR</button>
-        <button className={` m-1 p-1 ${descriptionType === 'less' ? selectedButtonStyle : undefined}`} onClick={() => setDescriptionType('less')}>Less</button>
-        <button className={` m-1 p-1 ${descriptionType === 'default' ? selectedButtonStyle : undefined}`} onClick={() => setDescriptionType('default')}>Default</button>
-        <button className={` m-1 p-1 ${descriptionType === 'more' ? selectedButtonStyle : undefined}`} onClick={() => setDescriptionType('more')}>More</button>
-      </div>
-      
-      {selectDescription(descriptionType)}
-      <Image src={profile} alt="profile picture" width={80} height={80} className="justify-self-center"/>
-    </div>
-  );
-}
 
 const Experience = () => {
-  const [descriptionType, setDescriptionType] = useState('default');
-  const [open, setOpen] = useState(false);
 
   return (
-    <div className="px-32 py-0" id="about">
+    <div className="px-32 py-0" id="experience">
       <main className="pt-8 px-0 flex flex-1 flex-col justify-start items-center">
-        <a onClick={() => setOpen(!open)} className="cursor-pointer">
-        <h1 className="text-5xl justify-self-center">
+        <h1 className="text-4xl justify-self-center mb-5">
           Experience
         </h1>
-        </a>
-        {open ? Description(descriptionType, setDescriptionType) : null}
+        {employments.map((employmentInfo: EmploymentInfo, idx: number) => {
+            return (
+              <EmploymentEntry {... employmentInfo} key={idx} />
+            );
+          })}
       </main>
     </div>
   );
