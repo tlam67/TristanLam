b7a30e92a9477ff2910a7aeb8c5167444f5ae5c1
tlam67 <tlamottawa@gmail.com>
2022-12-04 14:26:26 -0500

    removed broken dynamic grid column layout

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -9,7 +9,8 @@ const Projects = () => {
         <h1 className="text-4xl justify-self-center mb-5">
           Projects
         </h1>
-        <div className={`grid grid-cols-${projectData.length <= 2? projectData.length : 3} gap-4`}>
+        {/* <div className={`grid grid-cols-${projectData.length <= 2? projectData.length : 3} gap-4`}> */}
+        <div className={`grid grid-cols-3 gap-4`}>
         {projectData.map((entry: ProjectInfo, idx: number) => {
             return (
               <ProjectCard key={idx} {... entry}/>
