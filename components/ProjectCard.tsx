3537a5c8d210df38e75a28cfd67a701745a8504b
tlam67 <tlamottawa@gmail.com>
2022-12-03 21:13:21 -0500

    changed links to open new tab

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -33,11 +33,11 @@ const ProjectCard = ( projectInfo: ProjectInfo ) => {
               <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
           </a>
           {projectInfo.github? 
-            <a href={projectInfo.github} className="mr-1 mt-auto self-start inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <a href={projectInfo.github} target="_blank" rel="noopener noreferrer" className="mr-1 mt-auto self-start inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               Github
             </a> : <></>}
           {projectInfo.link? 
-          <a href={projectInfo.link} className="mr-1 mt-auto self-start inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <a href={projectInfo.link} target="_blank" rel="noopener noreferrer" className="mr-1 mt-auto self-start inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Website
           </a> : <></>}
         </div>
