d13eeaa102a31f9966c9f86a3b9931ed616d372c
tlam67 <tlamottawa@gmail.com>
2022-12-04 14:24:15 -0500

    fixed invalid rules

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -13,7 +13,7 @@ const ProjectCard = ( projectInfo: ProjectInfo ) => {
         <div className='mt-auto flex flex-row'>
           <a onClick={() => setShowModal(true)} className="mr-1 mt-auto self-start cursor-pointer inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black dark:text-white bg-amber-50 dark:bg-blue-700 rounded-lg hover:bg-amber-200 dark:hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               Read more
-              <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+              <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
           </a>
           {projectInfo.github? 
             <a href={projectInfo.github} target="_blank" rel="noopener noreferrer" className="mr-1 mt-auto self-start cursor-pointer inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black dark:text-white bg-amber-50 dark:bg-blue-700 rounded-lg hover:bg-amber-200 dark:hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -42,7 +42,7 @@ const ProjectCard = ( projectInfo: ProjectInfo ) => {
                           {projectInfo.title}
                       </h3>
                       <button type="button"  onClick={() => setShowModal(false)} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
-                          <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                          <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                       </button>
                   </div>
                   {/* <!-- Modal body --> */}
